6086a0f3355c2703d874d13881df3bc5cf5c128b
Brian Crafton <brian.crafton.research@gmail.com>
2026-03-17 15:24:53 -0700

    verify with random data

--- test_triton.py
+++ test_triton.py
@@ -1,5 +1,6 @@
 
 import torch
+import numpy as np
 
 from cs336_systems.fa1_triton import FlashAttentionTriton
 from cs336_systems.fa1_hack import FlashAttention
@@ -10,39 +11,31 @@ Q = torch.ones(size=(4, 128, 64)).to('cuda')
 K = torch.ones(size=(4, 128, 64)).to('cuda')
 V = torch.ones(size=(4, 128, 64)).to('cuda')
 
-# Q = torch.normal(mean=0., std=1., size=(4, 128, 64)).to('cuda')
-# K = torch.normal(mean=0., std=1., size=(4, 128, 64)).to('cuda')
-# V = torch.normal(mean=0., std=1., size=(4, 128, 64)).to('cuda')
+Q = torch.normal(mean=0., std=1., size=(4, 128, 64)).to('cuda')
+K = torch.normal(mean=0., std=1., size=(4, 128, 64)).to('cuda')
+V = torch.normal(mean=0., std=1., size=(4, 128, 64)).to('cuda')
 
 is_causal = False
 
-O = FlashAttention.apply(Q, K, V, is_causal)
-# print (O)
-print (O.shape)
-print (O[0][0][0])
+#################################
 
+O_ref = FlashAttention.apply(Q, K, V, is_causal)
 O = FlashAttentionTriton.apply(Q, K, V, is_causal)
-# print (O)
-print (O.shape)
-print (O[0][0][0])
 
-# print (O[1, :, :])
-
-# print (O[0,  0:16,  0:16])
-# print (O[0,  0:16, 16:32])
-
-# print (O[0,  0:16,  0:16])
-# print (O[0, 16:32,  0:16])
-# print (O[0, 32:48,  0:16])
-# print (O[0, 48:64,  0:16])
-# print (O[0, 64:80,  0:16])
-
+#################################
+'''
 for i in range(0, 128, 16):
   # print (O[1, i:i+16, 0:16])
   pass
 
 for i in range(0, 64, 16):
-  # print (O[1, 0:16, i:i+16])
+  #print (O[1, 0:16, i:i+16])
   pass
+'''
+#################################
+
+O_ref = O_ref.cpu().numpy()
+O = O.cpu().numpy()
+assert np.all(np.isclose(O, O_ref, atol=1e-2))
 
 #################################
